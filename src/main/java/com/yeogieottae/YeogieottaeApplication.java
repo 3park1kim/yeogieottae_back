efbcd4f6f4dbb21e8fb05480b4c0327d9787ddc9
jhpark0912 <jhpark9209@kakao.com>
2022-11-20 18:07:52 +0900

    :tada: [#1] chore: 백엔드 개발환경 기본 세팅

---
+++ src/main/java/com/yeogieottae/YeogieottaeApplication.java
@@ -0,0 +1,13 @@
+package com.yeogieottae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeogieottaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeogieottaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeogieottae/YeogieottaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeogieottae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeogieottaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
